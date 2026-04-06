623369a943278e072413d022105425a370a0876f
muncool39 <muncool39@gmail.com>
2026-04-06 16:00:24 +0900

    test: 통합 테스트

---
+++ src/test/java/com/example/my_api_server/service/OrderServiceIntegrationTest.java
@@ -0,0 +1,194 @@
+package com.example.my_api_server.service;
+
+
+import com.example.my_api_server.common.MemberFixture;
+import com.example.my_api_server.common.ProductFixture;
+import com.example.my_api_server.entity.Member;
+import com.example.my_api_server.entity.Product;
+import com.example.my_api_server.repo.MemberDBRepo;
+import com.example.my_api_server.repo.OrderProductRepo;
+import com.example.my_api_server.repo.OrderRepo;
+import com.example.my_api_server.repo.ProductRepo;
+import com.example.my_api_server.service.dto.OrderCreateDto;
+import com.example.my_api_server.service.dto.OrderResponseDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@SpringBootTest // Spring DI를 통해 빈(Bean) 주입헤주는 어노테이션 (Run해주는거랑 비슷)
+@ActiveProfiles("test") // yaml 파일 어떤거 읽을지
+public class OrderServiceIntegrationTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepo orderRepo;
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Autowired
+    private MemberDBRepo memberDBRepo;
+
+    @Autowired
+    private OrderProductRepo orderProductRepo;
+
+    @BeforeEach
+    public void setUp() {
+        orderProductRepo.deleteAllInBatch();
+        productRepo.deleteAllInBatch();
+        orderRepo.deleteAllInBatch();
+        memberDBRepo.deleteAllInBatch();
+    }
+    
+    private Member getSavedMember(String password) {
+        return memberDBRepo.save(MemberFixture
+                .defaultMember()
+                .password(password)
+                .build()
+        );
+    }
+
+    private List<Product> getProducts() {
+        return productRepo.saveAll(ProductFixture.defaultProducts());
+    }
+
+    private List<Long> getProductIds(List<Product> products) {
+        return products.stream()
+                .map(Product::getId)
+                .toList();
+    }
+
+    @Nested()
+    @DisplayName("주문 생성 TC")
+    class OrderCreateTest {
+        @Test
+        @DisplayName("주문 생성 시 DB에 저장되고 주문 시간이 Null이 아니다.")
+        public void createOrderPersistAndReturn(){
+            // given
+            List<Long> counts = List.of(1L, 2L);
+            Member savedMember = getSavedMember("1234");
+            List<Product> products = getProducts();
+            List<Long> productIds = getProductIds(products);
+            OrderCreateDto createDto = new OrderCreateDto(savedMember.getId(), productIds, counts);
+
+            // when
+            OrderResponseDto resDto = orderService.createOrder(createDto);
+
+            // then
+            assertThat(resDto.getOrderCompletedTime()).isNotNull();
+        }
+
+        @Test
+        @DisplayName("주문 생성 시 재고가 정상적으로 차감이 된다.")
+        public void createOrderStockDecreaseSuccess(){
+            // given
+            List<Long> counts = List.of(1L, 2L);
+            Member savedMember = getSavedMember("1234");
+            List<Product> products = getProducts();
+            List<Long> productIds = getProductIds(products);
+            OrderCreateDto createDto = new OrderCreateDto(savedMember.getId(), productIds, counts);
+
+            // when
+            OrderResponseDto resDto = orderService.createOrder(createDto);
+
+            // then
+            List<Product> resultProducts = productRepo.findAllById(productIds);
+
+            // 처음재고 - 주문재고 = 현재재고 (<- 가 잘 반영되었는지 확인) (주문 재고 반영 확인)
+            for (int i=0; i<products.size(); i++) {
+                Product beforProduct = products.get(i);
+                Product nowProduct = resultProducts.get(i);
+                Long orderStock = counts.get(i);
+
+                assertThat(beforProduct.getStock() - orderStock)
+                        .isEqualTo(nowProduct.getStock());
+            }
+        }
+
+        @Test
+        @DisplayName("주문 생성 시 재고가 부족하면 예외가 정상적으로 반횐된다.")
+        public void createOrderStockValidation(){
+            // given
+            List<Long> counts = List.of(10L, 20L);
+            Member savedMember = getSavedMember("1234");
+            List<Product> products = getProducts();
+            List<Long> productIds = getProductIds(products);
+
+            OrderCreateDto createDto = new OrderCreateDto(savedMember.getId(), productIds, counts);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> orderService.createOrder(createDto))
+                    .isInstanceOf(RuntimeException.class)
+                    .hasMessage("재고가 없어 주문 불가합니다.");
+        }
+
+        // 주문 생성시 상품 개수를 조회한다. (개수가 맞는지?)
+    }
+
+    @Nested()
+    @DisplayName("주문과 연관된 도메인 예외 TC")
+    class OrderRelatedExceptionTest {
+        @Test
+        @DisplayName("주문 시 회원이 존재하지 않으면 예외가 발생한다.")
+        public void validateMemberWhenCreateOrder(){
+            // given
+            List<Long> counts = List.of(1L, 2L);
+            Member savedMember = getSavedMember("1234");
+            List<Product> products = getProducts();
+            List<Long> productIds = getProductIds(products);
+
+            OrderCreateDto createDto = new OrderCreateDto(1234L, productIds, counts);
+
+            // when + then
+            assertThatThrownBy(() -> orderService.createOrder(createDto))
+                    .isInstanceOf(RuntimeException.class)
+                    .hasMessage("회원이 존재하지 않습니다.");
+
+        }
+
+        // 미니 과제 (케이스 4번)
+        @Test
+        @DisplayName("주문하는 상품에 대한 정보가 존재하지 않는 경우")
+        public void dd() {
+            return;
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
